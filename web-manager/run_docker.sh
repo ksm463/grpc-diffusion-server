aba87beca448b735dd466388be5c2e64043da15c
sungmin <ksm46351@gmail.com>
2025-07-17 09:39:26 +0900

    init web manager

---
+++ web-manager/run_docker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+port_num="2"
+CONTAINER_NAME="web-manager"
+NETWORK_NAME="diffusion-net"
+IMAGE_NAME="web-manager"
+TAG="0.1"
+
+fastapi_path=$(pwd)
+
+HOST_IP=$(hostname -I | awk '{print $1}')
+HOST_OS_VERSION=$(cat /etc/os-release | grep PRETTY_NAME | cut -d'=' -f2 | tr -d '"' || echo "N/A")
+HOST_TIMEZONE=$(timedatectl status | grep 'Time zone' | awk '{print $3 " (" $4 $5}' | sed 's/)//g; s/(//g' || cat /etc/timezone || echo "N/A")
+
+echo "--- Host System Information (to be passed to container) ---"
+echo "Host IP: $HOST_IP"
+echo "Host OS Version: $HOST_OS_VERSION"
+echo "Host Timezone: $HOST_TIMEZONE"
+echo "----------------------------------------------------------"
+
+
+docker run \
+    -d \
+    -p ${port_num}7000:7000 \
+    -p ${port_num}8000:8000 \
+    --name ${CONTAINER_NAME} \
+    --privileged \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ${fastapi_path}:/grpc_web_manager \
+    -e DISPLAY=$DISPLAY \
+    -e HOST_IP="$HOST_IP" \
+    -e HOST_OS_VERSION="$HOST_OS_VERSION" \
+    -e HOST_TIMEZONE="$HOST_TIMEZONE" \
+    --shm-size 20g \
+    --restart=always \
+    -w /grpc_web_manager \
+    ${IMAGE_NAME}:${TAG} \
+    tail -f /dev/null
+
+# docker network connect ${NETWORK_NAME} ${CONTAINER_NAME}
